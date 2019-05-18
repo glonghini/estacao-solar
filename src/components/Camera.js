187330dbb4826ca86c53c53b727580de11c541ab
glonghini <43152762+glonghini@users.noreply.github.com>
2019-05-18 13:30:54 -0300

    Camera 30s fixed

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -3,33 +3,23 @@ import axios from 'axios';
 
 export class Camera extends React.Component {
   state = {
-    cameraLink: ""
+    cameraLink: 'http://143.107.235.2:8000/sensors/media/',
+    imageHash: Date.now()
   }
   cameraRefresh = () => {
     this.setState({
-      cameraLink: '',
-      cameraLink: "http://143.107.235.2:8000/sensors/media/"
+      imageHash: Date.now()
     });
-    this.forceUpdate();
     setTimeout(this.cameraRefresh, 30*1000);
   }
   componentDidMount(){
-    axios.get("http://143.107.235.2:8000/sensors/media/")
-      .then(response => {
-        this.setState(
-          {
-          cameraLink: response.data
-          }
-        )
-        console.log(response.data);
-      }
-    );
+    this.cameraRefresh();
   }
   render(){
     return(
       <div className='camera-image'>
         <img
-        src={this.state.cameraLink}
+        src={`${this.state.cameraLink}?${this.state.imageHash}`}
         width="100%"
         height="100%"
         alt="camera feed"
